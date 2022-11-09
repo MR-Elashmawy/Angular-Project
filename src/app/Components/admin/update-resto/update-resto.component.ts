5f4cbc3f81437827c69acf8e80a7109277443906
tarekelmogyy <elmogyt266@gmail.com>
2022-11-09 07:31:23 +0200

    View,add,edit,delete service

--- src/app/Components/admin/update-resto/update-resto.component.ts
+++ src/app/Components/admin/update-resto/update-resto.component.ts
@@ -9,12 +9,32 @@ import { RestoService } from 'src/app/resto.service';
   styleUrls: ['./update-resto.component.css']
 })
 export class UpdateRestoComponent implements OnInit {
+  alert:boolean=false;
   editResto = new FormGroup({
     name : new FormControl(''),
     description : new FormControl(''),
     address : new FormControl(''),
   })
-  constructor(private router:ActivatedRoute,private resto:RestoService) { }
+  constructor(private router:ActivatedRoute,private resto:RestoService) {
+    //console.warn(this.router.snapshot.params['id']);
+    this.resto.getCurrentResto(this.router.snapshot.params['id']).subscribe((result:any)=>{
+      this.editResto = new FormGroup({
+        name : new FormControl(result['name']),
+        description : new FormControl(result['description']),
+        address : new FormControl(result['address']),
+      })
+    })
+   }
+   collection(){
+   // console.warn(this.editResto.value);
+    this.resto.updateResto(this.router.snapshot.params['id'],this.editResto.value).subscribe((result)=>{
+      //console.warn(result);
+      this.alert=true;
+    })
+   }
+   closeAlert(){
+    this.alert=false;
+   }
 
   ngOnInit(): void {
 
